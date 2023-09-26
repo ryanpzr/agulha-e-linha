5a5806ea75aff725f27761b304161563c333af1f
Ryan <ryanpereiralimads@gmail.com>
2023-09-25 21:29:49 -0300

    env

--- docs/backend/index.js
+++ docs/backend/index.js
@@ -47,7 +47,6 @@ server.get('/get', (req, res) => {
 
 server.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'agulha-e-linha.up.railway.app/');
-    res.setHeader('Access-Control-Allow-Origin', 'localhost:3000/');
     // Outros cabeçalhos CORS, métodos permitidos, etc.
     next();
 });
